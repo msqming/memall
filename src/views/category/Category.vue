bba7f60bbc84de537be3796f65c5b5b454a1b04c
msqming <msqming@163.com>
2019-10-21 19:10:23 +0800

    初始化项目

---
+++ src/views/category/Category.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>分类</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Category",
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    height: 150px;
+    background-color: red;
+
+    overflow: hidden;
+    /*overflow-y: scroll;*/
+  }
+</style>
